756f9a75371c21e0e0581cad9885732d8be90190
Himanshu Patel <44549418+patel-himanshu@users.noreply.github.com>
2023-02-02 10:25:05 +0530

    Starter project setup

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    StarComponent,
+    ConvertToSpacesPipe,
+  ],
+  imports: [BrowserModule, FormsModule],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
