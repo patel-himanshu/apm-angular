3199527427aa701dca7faa40cee99bf9c0870aa2
Himanshu Patel <44549418+patel-himanshu@users.noreply.github.com>
2023-02-04 22:30:15 +0530

    Create Product Detail component

---
+++ src/app/products/product-detail/product-detail.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pm-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css'],
+})
+export class ProductDetailComponent implements OnInit {
+  pageTitel: string = 'Product Detail';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
