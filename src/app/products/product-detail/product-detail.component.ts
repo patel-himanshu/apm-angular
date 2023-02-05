28ca257fdf04a768aa5980c76fcfa83229cd458d
Himanshu Patel <44549418+patel-himanshu@users.noreply.github.com>
2023-02-05 23:02:34 +0530

    Add info in the product detail page

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,17 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { IProduct } from '../product';
+import { ProductService } from '../product.services';
 
 @Component({
   selector: 'pm-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css'],
 })
-export class ProductDetailComponent implements OnInit {
+export class ProductDetailComponent implements OnInit, OnDestroy {
   pageTitle: string = 'Product Detail';
   product: IProduct | undefined;
+  errorMessage: string = '';
+  sub!: Subscription;
 
-  constructor(private router: Router, private route: ActivatedRoute) {}
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {}
 
   onBack(): void {
     this.router.navigate(['/products']);
@@ -19,5 +27,14 @@ export class ProductDetailComponent implements OnInit {
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.sub = this.productService.getProducts().subscribe({
+      next: (products) =>
+        (this.product = products.find((product) => product.productId === id)),
+      error: (err) => (this.errorMessage = err),
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
   }
 }
